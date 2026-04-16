4bba63fbfa0dc9fec53dec5490959a5e36248614
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-04-16 15:00:39 -0400

    Remove vaporwave borders, widen speech bubble, fix Dropline font

--- components/speech-bubble.js
+++ components/speech-bubble.js
@@ -6,16 +6,16 @@ const SpeechBubble = () => {
       position="relative"
       bg={useColorModeValue('rgba(0,0,0,0.6)', 'rgba(255,255,255,0.1)')}
       color="white"
-      px={4}
-      py={2}
+      px={6}
+      py={3}
       borderRadius="lg"
-      maxW="350px"
+      maxW="640px"
       mx="auto"
       mt={4}
       mb={6}
       backdropFilter="blur(10px)"
     >
-      <Text fontSize="sm" textAlign="center">
+      <Text fontSize="md" textAlign="center">
         Hello, I&apos;m a computational systems engineer based in Kitchener!
       </Text>
     </Box>
