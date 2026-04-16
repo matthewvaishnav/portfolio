c62d5ec0323f9eac11caf203d868941adb92043a
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-04-16 12:54:34 -0400

    Update I ♥ section: replace piano with coffee

--- pages/index.js
+++ pages/index.js
@@ -118,7 +118,7 @@ const Home = () => (
           I ♥
         </Heading>
         <Paragraph>
-          Art, music, playing piano, my dog, and machine learning.
+          Art, music, my dog, coffee, and machine learning.
         </Paragraph>
       </Section>
 
