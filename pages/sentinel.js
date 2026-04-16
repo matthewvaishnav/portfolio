3126c8037067cc62d87319fbef2bd26be69fdcdb
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-04-16 14:47:42 -0400

    Add 3D laptop model to all project pages

--- pages/sentinel.js
+++ pages/sentinel.js
@@ -3,16 +3,36 @@ import {
   Badge,
   Link,
   List,
-  ListItem
+  ListItem,
+  Box
 } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../components/work'
 import P from '../components/paragraph'
 import Layout from '../components/layouts/article'
 
+const LazyVoxelDog = dynamic(() => import('../components/voxel-dog'), {
+  ssr: false,
+  loading: () => <DogSpinner />
+})
+
+import dynamic from 'next/dynamic'
+import { DogSpinner } from '../components/voxel-dog-loader'
+
 const Work = () => (
   <Layout title="SENTINEL">
     <Container>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg="whiteAlpha.200"
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
+        <LazyVoxelDog />
+      </Box>
+
       <Title>
         SENTINEL <Badge>2026</Badge>
       </Title>
