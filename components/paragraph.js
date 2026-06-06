1ed693cbf74cb6007430b121cd27009115c03e21
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-06-06 15:05:22 -0400

    Normalize paragraph alignment across portfolio

--- components/paragraph.js
+++ components/paragraph.js
@@ -1,8 +1,20 @@
 import styled from '@emotion/styled'
 
 const Paragraph = styled.p`
+  display: block;
+  width: 100%;
+  max-width: 100%;
+  margin: 0 0 1rem 0;
+  padding: 0;
   text-align: left;
-  hyphens: auto;
+  text-indent: 0;
+  hyphens: none;
+  overflow-wrap: break-word;
+  word-break: normal;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
 `
 
 export default Paragraph
