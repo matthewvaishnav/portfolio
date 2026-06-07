c761d42b42cbd2edeb0c3cc7746b2a05668700b8
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-06-07 11:07:48 -0400

    Revert unintended portfolio paragraph change

--- components/paragraph.js
+++ components/paragraph.js
@@ -1,20 +1,8 @@
 import styled from '@emotion/styled'
 
 const Paragraph = styled.p`
-  display: block;
-  width: 100%;
-  max-width: 100%;
-  margin: 0 0 1rem 0;
-  padding: 0;
   text-align: left;
-  text-indent: 0;
-  hyphens: none;
-  overflow-wrap: break-word;
-  word-break: normal;
-
-  &:last-child {
-    margin-bottom: 0;
-  }
+  hyphens: auto;
 `
 
 export default Paragraph
