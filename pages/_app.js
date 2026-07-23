ea78023beff3eb1f34f9ca03c3526c56b777ff29
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-07-23 17:53:44 -0400

    Remove the posts section (#4)
    Remove all post routes, blog-only components, navigation entries, and obsolete blog routing while pointing the homepage research CTA to the dedicated Research library.

--- pages/_app.js
+++ pages/_app.js
@@ -4,26 +4,6 @@ import theme from '../lib/theme'
 import { AnimatePresence } from 'framer-motion'
 
 function Website({ Component, pageProps, router }) {
-  const isBlogRoute = router.route.startsWith('/posts')
-  
-  if (isBlogRoute) {
-    return (
-      <ChakraProvider theme={theme}>
-        <AnimatePresence
-          mode="wait"
-          initial={true}
-          onExitComplete={() => {
-            if (typeof window !== 'undefined') {
-              window.scrollTo({ top: 0 })
-            }
-          }}
-        >
-          <Component {...pageProps} key={router.route} />
-        </AnimatePresence>
-      </ChakraProvider>
-    )
-  }
-
   return (
     <ChakraProvider theme={theme}>
       <Layout router={router}>
