3b3b2d95607ca67b1e0385df7af66c27a286ef78
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-09-16 10:03:20 -0400

    Fix navbar lint so persistent laptop layout can deploy

--- components/navbar.js
+++ components/navbar.js
@@ -42,6 +42,7 @@ const LinkItem = ({ href, path, target, children, ...props }) => {
 const MenuLink = forwardRef((props, ref) => (
   <Link ref={ref} as={NextLink} {...props} />
 ))
+MenuLink.displayName = 'MenuLink'
 
 const Navbar = props => {
   const { path } = props
