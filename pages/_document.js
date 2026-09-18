16503acdf846d12890a5266917df9e0365ce04bc
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-09-18 12:25:28 -0400

    Load rounded navbar font

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap"
+          rel="stylesheet"
+        />
         <style>{`
           @font-face {
             font-family: 'Dropline';
