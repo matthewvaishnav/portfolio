70c2d37412b15ec2a3ba6133c25c85a9006f6963
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-09-18 10:07:19 -0400

    Harden footer semantics and links

--- components/footer.js
+++ components/footer.js
@@ -1,34 +1,37 @@
 import { Box, Link, Stack, Wrap, WrapItem } from '@chakra-ui/react'
 
-const Footer = () => {
-  return (
-    <Box align="center" opacity={0.4} fontSize="sm" mt={10}>
-      <Stack spacing={2} align="center">
-        <Box>Matthew Vaishnav | Computer Programming @ Conestoga | 2026–2027</Box>
-        <Box>Kitchener-Waterloo, Ontario</Box>
-        <Wrap justify="center" spacing={{ base: 3, md: 4 }}>
-          <WrapItem>
-            <Link href="https://github.com/matthewvaishnav" target="_blank">
-              GitHub
-            </Link>
-          </WrapItem>
-          <WrapItem>
-            <Link
-              href="https://www.linkedin.com/in/matthew-vaishnav-594312403/"
-              target="_blank"
-            >
-              LinkedIn
-            </Link>
-          </WrapItem>
-          <WrapItem>
-            <Link href="mailto:matthewvaishnav@gmail.com" wordBreak="break-word">
-              matthewvaishnav@gmail.com
-            </Link>
-          </WrapItem>
-        </Wrap>
-      </Stack>
-    </Box>
-  )
-}
+const Footer = () => (
+  <Box as="footer" textAlign="center" opacity={0.55} fontSize="sm" mt={10}>
+    <Stack spacing={2} align="center">
+      <Box>Matthew Vaishnav | Computer Programming @ Conestoga | 2026–2027</Box>
+      <Box>Kitchener-Waterloo, Ontario</Box>
+      <Wrap justify="center" spacing={{ base: 3, md: 4 }}>
+        <WrapItem>
+          <Link
+            href="https://github.com/matthewvaishnav"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </Link>
+        </WrapItem>
+        <WrapItem>
+          <Link
+            href="https://www.linkedin.com/in/matthew-vaishnav-594312403/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </Link>
+        </WrapItem>
+        <WrapItem>
+          <Link href="mailto:matthewvaishnav@gmail.com" wordBreak="break-word">
+            matthewvaishnav@gmail.com
+          </Link>
+        </WrapItem>
+      </Wrap>
+    </Stack>
+  </Box>
+)
 
 export default Footer
