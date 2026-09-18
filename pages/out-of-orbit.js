0baf3154950f6dc5c7064f53edf5adbd1e15deea
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-09-18 10:10:02 -0400

    Audit pages/out-of-orbit.js for accurate public claims

--- pages/out-of-orbit.js
+++ pages/out-of-orbit.js
@@ -17,16 +17,22 @@ const Work = () => (
         Out of Orbit <Badge>2025</Badge>
       </Title>
       <P>
-        Vaporwave-themed arcade shooter with optimized object pooling and retro aesthetics.
+        A C++ arcade shoot-em-up built with Raylib, featuring fixed-size object pools, multiple ship archetypes, escalating waves, bosses, and a data-driven upgrade loop.
       </P>
       <List ml={4} my={4}>
         <ListItem>
+          <Meta>Website</Meta>
+          <Link href="https://github.com/matthewvaishnav/out-of-orbit" target="_blank" rel="noopener noreferrer">
+            GitHub repository <ExternalLinkIcon mx="2px" />
+          </Link>
+        </ListItem>
+        <ListItem>
           <Meta>Platform</Meta>
-          <span>Windows/macOS/Linux</span>
+          <span>Linux; portable C++17/Raylib codebase</span>
         </ListItem>
         <ListItem>
           <Meta>Stack</Meta>
-          <span>Unity, C#, Shader Graph</span>
+          <span>C++17, Raylib</span>
         </ListItem>
       </List>
     </Container>
