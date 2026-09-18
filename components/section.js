d8e341e9be3a78a571203ec3a2ef9286116e3b19
Matthew Vaishnav <matthewvaishnav@gmail.com>
2026-09-18 10:13:11 -0400

    Respect reduced motion in section transitions

--- components/section.js
+++ components/section.js
@@ -1,21 +1,26 @@
-import { motion } from 'framer-motion'
 import { chakra, shouldForwardProp } from '@chakra-ui/react'
+import { motion, useReducedMotion } from 'framer-motion'
 
 const StyledDiv = chakra(motion.div, {
-  shouldForwardProp: prop => {
-    return shouldForwardProp(prop) || prop === 'transition'
-  }
+  shouldForwardProp: prop => shouldForwardProp(prop) || prop === 'transition'
 })
 
-const Section = ({ children, delay = 0 }) => (
-  <StyledDiv
-    initial={{ y: 10, opacity: 0 }}
-    animate={{ y: 0, opacity: 1 }}
-    transition={{ duration: 0.8, delay }}
-    mb={{ base: 8, md: 10 }}
-  >
-    {children}
-  </StyledDiv>
-)
+const Section = ({ children, delay = 0 }) => {
+  const reduceMotion = useReducedMotion()
+
+  return (
+    <StyledDiv
+      initial={reduceMotion ? false : { y: 10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{
+        duration: reduceMotion ? 0 : 0.6,
+        delay: reduceMotion ? 0 : delay
+      }}
+      mb={{ base: 8, md: 10 }}
+    >
+      {children}
+    </StyledDiv>
+  )
+}
 
 export default Section
